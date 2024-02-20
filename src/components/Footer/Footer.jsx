0e17b9d54898eea05a40670b948c0d8bc368bef3
maria ryabikovskaya <ryabikovskay@gmail.com>
2024-02-20 15:08:51 +0300

    feat: add footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,32 @@
+import { Box, Link, Typography } from "@mui/material";
+import s from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <Box className={s.wrapp} id="contacts">
+      <Box>
+        <Typography className={s.rights}>2024, Все права защищены</Typography>
+      </Box>
+      <Box>
+        <Box>
+          <Link href="https://t.me/+375296710970">
+            <img alt="" src={require("./img/tg.png")} className={s.logos} />
+          </Link>
+          <Link href="https://vk.com/id357926171">
+            <img alt="" src={require("./img/vk.png")} className={s.logos} />
+          </Link>
+        </Box>
+        <Box>
+          <Link href="https://wa.me/qr/N4ZHU5HHCE5WH1">
+            <img alt="" src={require("./img/wa.png")} className={s.logos} />
+          </Link>
+          <Link href="https://www.instagram.com/liliyasauchanka/?hl=ru">
+            <img alt="" src={require("./img/inst.png")} className={s.logos} />
+          </Link>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default Footer;
