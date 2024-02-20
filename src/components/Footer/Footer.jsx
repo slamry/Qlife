681db710b425581949c4f75ce9dfff7594a5514c
maria ryabikovskaya <ryabikovskay@gmail.com>
2024-02-20 19:58:08 +0300

    fix: icons = line

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,25 +5,23 @@ const Footer = () => {
   return (
     <Box className={s.wrapp} id="contacts">
       <Box>
-        <Typography className={s.rights}>2024, Все права защищены</Typography>
+        <Typography className={s.rights}>
+          2024, Все права <br /> защищены
+        </Typography>
       </Box>
       <Box>
-        <Box>
-          <Link href="https://t.me/+375296710970">
-            <img alt="" src={require("./img/tg.png")} className={s.logos} />
-          </Link>
-          <Link href="https://vk.com/id357926171">
-            <img alt="" src={require("./img/vk.png")} className={s.logos} />
-          </Link>
-        </Box>
-        <Box>
-          <Link href="https://wa.me/qr/N4ZHU5HHCE5WH1">
-            <img alt="" src={require("./img/wa.png")} className={s.logos} />
-          </Link>
-          <Link href="https://www.instagram.com/liliyasauchanka/?hl=ru">
-            <img alt="" src={require("./img/inst.png")} className={s.logos} />
-          </Link>
-        </Box>
+        <Link href="https://t.me/+375296710970">
+          <img alt="" src={require("./img/tg.png")} className={s.logos} />
+        </Link>
+        <Link href="https://vk.com/id357926171">
+          <img alt="" src={require("./img/vk.png")} className={s.logos} />
+        </Link>
+        <Link href="https://wa.me/qr/N4ZHU5HHCE5WH1">
+          <img alt="" src={require("./img/wa.png")} className={s.logos} />
+        </Link>
+        <Link href="https://www.instagram.com/liliyasauchanka/?hl=ru">
+          <img alt="" src={require("./img/inst.png")} className={s.logos} />
+        </Link>
       </Box>
     </Box>
   );
