38aa0344b5748691464621e3c4a8703b76d8cfc2
maria ryabikovskaya <ryabikovskay@gmail.com>
2024-02-24 13:11:10 +0300

    feat: add form adaptive

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,16 +1,21 @@
 import {
+  Box,
   Grid,
   Typography,
   // Input,
   // FormControl,
   // FormHelperText,
   // Button,
-  Box,
 } from "@mui/material";
 import s from "./Form.module.css";
+import useWindowSize from "../common/useWindowSize/useWindowSize";
+
 // import { useForm } from "react-hook-form";
 
 const Form = () => {
+  // eslint-disable-next-line
+  const { width, height } = useWindowSize();
+
   // const {
   //   register,
   //   handleSubmit,
@@ -45,20 +50,37 @@ const Form = () => {
 
   return (
     <Grid container className={s.wrapp}>
-      <Grid item xs={5} className={s.content_wrapp}>
-        <Typography className={s.title}>
-          Хватит существовать без плана!
-        </Typography>
-        <Typography className={s.content}>
-          Хотите узнать, чего вам не хватает, чтобы жить в гармонии с собой и
-          достигать целей?
-        </Typography>
-        <img className={s.game} alt="" src={require("./img/game.png")} />
-        <img className={s.apple} alt="" src={require("./img/apple.png")} />
-      </Grid>
-      <Grid item xs={7} className={s.form_wrapp} id="form">
-        <Box className={s.colored}>
+      {width > 956 ? (
+        <Grid item xs={4.5} className={s.content_wrapp}>
+          <Typography className={s.title}>
+            Хватит существовать без плана!
+          </Typography>
+          <Typography className={s.content}>
+            Хотите узнать, чего вам не хватает, чтобы жить в гармонии с собой и
+            достигать целей?
+          </Typography>
+
+          <img className={s.game} alt="" src={require("./img/game.png")} />
+          <img className={s.apple} alt="" src={require("./img/apple.png")} />
+        </Grid>
+      ) : (
+        <Grid item xs={12} className={s.content_wrapp}>
+          <Box className={s.text_wrapp}>
+            <Typography className={s.title}>
+              Хватит существовать без плана!
+            </Typography>
+            <Typography className={s.content}>
+              Хотите узнать, чего вам не хватает, чтобы жить в гармонии с собой
+              и достигать целей?
+            </Typography>
+          </Box>
+          <img className={s.apple} alt="" src={require("./img/apple.png")} />
+        </Grid>
+      )}
+      {width > 956 ? (
+        <Grid item xs={7.5} className={s.form_wrapp} id="form">
           <iframe
+            scrolling="no"
             title="form"
             src="https://docs.google.com/forms/d/e/1FAIpQLSd0RleaOAAZuypfgV654ozcNhUzNvY3w7v2cyFrAp8QHrtmXQ/viewform?embedded=true"
             frameBorder="0"
@@ -66,86 +88,25 @@ const Form = () => {
           >
             Загрузка…
           </iframe>
-        </Box>
-
-        {/* <form onSubmit={handleSubmit(onSubmit)} className={s.form_}>
-          <Typography className={s.f_title}>
-            Оставьте заявку, и мы вышлем дату, а также место проведения игры в
-            вашем городе
-          </Typography>
-          <FormControl className={s.form_control}>
-            <Input
-              inputProps={{ maxLength: 100, minLength: 3 }}
-              placeholder="Фамилия Имя Отчество"
-              className={s.input}
-              {...register("user_name", {
-                required: "Обязательно для заполнения",
-              })}
-            />
-            <FormHelperText error={!!errors.user_name}>
-              {errors.user_name?.message}
-            </FormHelperText>
-          </FormControl>
-          <FormControl className={s.form_control}>
-            <Input
-              inputProps={{ maxLength: 100, minLength: 3 }}
-              placeholder="Номер телефона"
-              className={s.input}
-              {...register("user_pnumber", {
-                required: "Обязательно для заполнения",
-                pattern: {
-                  value: /^\+\d{1,3}-\d{2}-\d{7}$/,
-                  message:
-                    "Введите корректный номер телефона в формате +XXX-XX-XXXXXXX",
-                },
-              })}
-            />
-            <FormHelperText error={!!errors.user_pnumber}>
-              {errors.user_pnumber?.message}
-            </FormHelperText>
-          </FormControl>
-          <FormControl className={s.form_control}>
-            <Input
-              inputProps={{ maxLength: 100, minLength: 3 }}
-              placeholder="Город"
-              className={s.input}
-              {...register("user_city", {
-                required: "Обязательно для заполнения",
-              })}
-            />
-            <FormHelperText error={!!errors.user_city}>
-              {errors.user_city?.message}
-            </FormHelperText>
-          </FormControl>
-          <FormControl className={s.form_control}>
-            <Input
-              inputProps={{ maxLength: 100, minLength: 3 }}
-              placeholder="Электронная почта"
-              className={s.input}
-              {...register("user_email", {
-                required: "Обязательно для заполнения",
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                  message: "Введите корректный адрес электронной почты",
-                },
-              })}
-            />
-            <FormHelperText error={!!errors.user_email}>
-              {errors.user_email?.message}
-            </FormHelperText>
-          </FormControl>
-          <Box className={s.butt_wrapp}>
-            <Button
-              className={s.butt}
-              type="submit"
-              // onClick={(e) => e.preventDefault()}
-            >
-              Записаться на игру
-            </Button>
+          <img className={s.cubes} alt="" src={require("./img/cubes.png")} />
+        </Grid>
+      ) : (
+        <Grid item xs={12} className={s.form_wrapp} id="form">
+          <iframe
+            scrolling="no"
+            title="form"
+            src="https://docs.google.com/forms/d/e/1FAIpQLSd0RleaOAAZuypfgV654ozcNhUzNvY3w7v2cyFrAp8QHrtmXQ/viewform?embedded=true"
+            frameBorder="0"
+            className={s.form}
+          >
+            Загрузка…
+          </iframe>
+          <img className={s.cubes} alt="" src={require("./img/cubes.png")} />
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <img className={s.game} alt="" src={require("./img/game.png")} />
           </Box>
-        </form> */}
-        <img className={s.cubes} alt="" src={require("./img/cubes.png")} />
-      </Grid>
+        </Grid>
+      )}
     </Grid>
   );
 };
