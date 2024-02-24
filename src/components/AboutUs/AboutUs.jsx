df23850b41958029282038718c18f6164201418b
maria ryabikovskaya <ryabikovskay@gmail.com>
2024-02-24 19:39:37 +0300

    feat: add adaptive to about us

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,10 +1,13 @@
 import { Grid, Typography, Box } from "@mui/material";
 import s from "./AboutUs.module.css";
+import useWindowSize from "../common/useWindowSize/useWindowSize";
 
 const AboutUs = () => {
+  // eslint-disable-next-line
+  const { width, height } = useWindowSize();
   return (
     <Grid container className={s.grid_c}>
-      <Grid item xs={5.5}>
+      <Grid item xs={width > 957 ? 6 : 12}>
         <Typography className={s.hi_text}>
           “Всем привет! Меня зовут Лилия Савченко”
         </Typography>
@@ -25,41 +28,82 @@ const AboutUs = () => {
           Если вы хотите жить качественной жизнью, научитесь равномерно
           заполнять все ее сферы. В этом вам поможет игра Quality life!
         </Typography>
-        <Box className={s.rect}>
-          <Typography className={s.yougot}>Вы получите:</Typography>
-          <Typography className={s.point}>
-            <img
-              src={require("./img/marker.png")}
-              alt=""
-              className={s.marker}
-            />
-            Новое окружение
-          </Typography>
-          <Typography className={s.point}>
-            <img
-              src={require("./img/marker.png")}
-              alt=""
-              className={s.marker}
-            />
-            Интересные знакомства
-          </Typography>
-          <Typography className={s.point}>
-            <img
-              src={require("./img/marker.png")}
-              alt=""
-              className={s.marker}
-            />
-            Диагностику баланса сфер своей жизни в легкой игровой форме
-          </Typography>
-        </Box>
+        {width > 957 ? (
+          <Box className={s.rect}>
+            <Typography className={s.yougot}>Вы получите:</Typography>
+            <Typography className={s.point}>
+              <img
+                src={require("./img/marker.png")}
+                alt=""
+                className={s.marker}
+              />
+              Новое окружение
+            </Typography>
+            <Typography className={s.point}>
+              <img
+                src={require("./img/marker.png")}
+                alt=""
+                className={s.marker}
+              />
+              Интересные знакомства
+            </Typography>
+            <Typography className={s.point}>
+              <img
+                src={require("./img/marker.png")}
+                alt=""
+                className={s.marker}
+              />
+              Диагностику баланса сфер своей жизни в легкой игровой форме
+            </Typography>
+          </Box>
+        ) : null}
       </Grid>
-      <Grid item xs={6.5} className={s.grid_images}>
-        <img src={require("./img/mbag.png")} alt="" className={s.mbag} />
+      {width > 957 ? (
+        <Grid item xs={1} className={s.mbag_wrapp}>
+          <img src={require("./img/mbag.png")} alt="" className={s.mbag} />
+        </Grid>
+      ) : null}
+      <Grid item xs={width > 957 ? 5 : 12} className={s.grid_images}>
+        <img src={require("./img/liliya.png")} alt="" className={s.liliya} />
         <Box className={s.text_wrapp}>
           <Typography className={s.text}>
             Приходите к нам на игру, чтобы провести время весело и с пользой!
           </Typography>
         </Box>
+        {width > 957 ? null : (
+          <Box sx={{display:'flex', flexWrap:'nowrap'}}>
+            <Box className={s.rect}>
+              <Typography className={s.yougot}>Вы получите:</Typography>
+              <Typography className={s.point}>
+                <img
+                  src={require("./img/marker.png")}
+                  alt=""
+                  className={s.marker}
+                />
+                Новое окружение
+              </Typography>
+              <Typography className={s.point}>
+                <img
+                  src={require("./img/marker.png")}
+                  alt=""
+                  className={s.marker}
+                />
+                Интересные знакомства
+              </Typography>
+              <Typography className={s.point}>
+                <img
+                  src={require("./img/marker.png")}
+                  alt=""
+                  className={s.marker}
+                />
+                Диагностику баланса сфер своей жизни в легкой игровой форме
+              </Typography>
+            </Box>
+            <Box className={s.mbag_wrapp}>
+              <img src={require("./img/mbag.png")} alt="" className={s.mbag} />
+            </Box>
+          </Box>
+        )}
       </Grid>
     </Grid>
   );
